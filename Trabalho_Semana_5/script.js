6969fbf70cff81a00768a2bd2001c5318c2c0d77
DANIEL MAFFEZZOLI <202308507702@alunos.ibmec.edu.br>
2024-03-27 11:12:10 -0300

    trabalho semana 5

--- Trabalho_Semana_5/script.js
+++ Trabalho_Semana_5/script.js
@@ -1,7 +1,39 @@
-var escopo = "Oi"
+var escopo = "Normal"
 
 function var_escopo(){
-    var escopo = "Mudei"
+    escopo = "Mudei"
 }
+
 var_escopo()
 console.log(escopo)
+
+let escopo_let = "Oi"
+
+function let_escopo(){
+  let escopo_let = "Mudei_let"
+  console.log(escopo_let)
+}
+
+let_escopo()
+console.log(escopo_let)
+
+const escopo_const = "Constante"
+
+function const_escopo(){
+  const escopo_const = "Constante_função"
+  console.log(escopo_const)
+}
+const_escopo()
+//escopo_const = "Nova const"; Constante não pode ser redeclarada
+console.log(escopo_const)
+
+var notas = [1,2]
+console.log(notas)
+notas.push(3) //Adiciona um 3 no final da lista
+console.log(notas)
+notas.pop() //Retira o elemento mais a direita da lista
+console.log(notas)
+notas.shift() //Retira o elemento mais a esquerda da lista
+console.log(notas)
+notas.unshift(1) //Adiciona um elemento no início da lista
+console.log(notas)
